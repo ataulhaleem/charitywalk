f7a2195822cf24752d6e01da6bb714a386838d80
ataulhaleem <ataulhaleem@gmail.com>
2024-03-18 21:34:27 +0100

    config chagnes

--- next.config.mjs
+++ next.config.mjs
@@ -7,26 +7,26 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output:'export',
-  basePath: '/charitywalk',
+  // basePath: '/charitywalk',
   reactStrictMode : true,
-    webpack: (config) => {
-      config.module.rules.push({
-        test: /\.(node)$/i,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              publicPath: '/_next',
-              outputPath: 'dist/node',
-              name: '[name].[ext]',
-              esModule: false,
-            },
-          },
-        ],
-      });
+  // webpack: (config) => {
+  //     config.module.rules.push({
+  //       test: /\.(node)$/i,
+  //       use: [
+  //         {
+  //           loader: 'file-loader',
+  //           options: {
+  //             publicPath: '/_next',
+  //             outputPath: 'dist/node',
+  //             name: '[name].[ext]',
+  //             esModule: false,
+  //           },
+  //         },
+  //       ],
+  //     });
   
-      return config;
-    },
+    //   return config;
+    // },
   };
   
   export default nextConfig;
